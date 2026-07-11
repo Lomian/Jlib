eb93d3cf074589f1c1028ad53dd44809adddda59
Lomian <ywoo7077@gmail.com>
2026-07-11 14:26:41 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,9 @@
 use pyo3::prelude::*;
 
 
-#[pyfunction]
-
-fn vec(a: Vec<i32>) {
-
-    return;
-
-}
 
 
 
-
-
-
-
-
-
-
-
-
- //   fn add(a: Vec<i32>, b: Vec<i32>) -> Vec<i32>  {
-
-   //     a.iter()
-     //       .zip(b.iter())
-       //     .map(|(x, y)| x + y)
-       //     .collect()
-
-
-  //  }
 
 
 
@@ -41,14 +16,8 @@ fn vec(a: Vec<i32>) {
 
 fn main() {
 
-    let bor = add(vec![1, 2, 3], vec![1, 2, 3]);
-    println!("{:?}", bor)
+
 
 
 }
 
-#[pymodule]
-fn jlib(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(vec, m)?)?;
-    Ok(())
-}
